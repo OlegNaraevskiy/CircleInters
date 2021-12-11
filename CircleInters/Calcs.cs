55054d2385c72a014fdecb3f5dcbbaf552cc83f4
Oleg Naraevskiy <noa.oleg96@gmail.com>
2021-12-11 06:59:08 +0300

    * job done

---
+++ CircleInters/Calcs.cs
@@ -0,0 +1,49 @@
+﻿/*===================================================================
+ * Copyright (c) 2021 Oleg Naraevskiy                   Date: 12.2021
+ * Version IDE: MS VS 2019
+ * Designed by: Oleg Naraevskiy / noa.oleg96@gmail.com      [12.2021]
+ *===================================================================*/
+
+namespace CircleInters
+{
+	public class Calcs
+	{
+		public int GetOccurrences(int[] a)
+		{
+			int result = 0;
+
+			int[] startPoints = new int[a.Length];
+			int[] endPoints = new int[a.Length];
+
+			int size = a.Length - 1;
+
+			for (int i = 0; i < a.Length; i++)
+			{
+				int start = i > a[i] ? i - a[i] : 0;
+				int end = size - i > a[i] ? i + a[i] : size;
+				startPoints[start] = startPoints[start] + 1;
+				endPoints[end] = endPoints[end] + 1;
+			}
+
+			int j = 0;
+			for (int i = 0; i < a.Length; i++)
+			{
+				if (startPoints[i] > 0)
+				{
+					result += j * startPoints[i];
+					result += startPoints[i] * (startPoints[i] - 1) / 2;
+
+					if (result > 10000000)
+					{
+						return -1;
+					}
+
+					j += startPoints[i]; 
+				}
+				j -= endPoints[i];
+			}
+
+			return result;
+		}
+	}
+}
--- CircleInters/Program.cs
+++ CircleInters/Program.cs
@@ -6,7 +6,29 @@ namespace CircleInters
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Calcs calc = new Calcs();
+
+			int[] testA = new int[] { 1, 5, 2, 1, 4, 0 };
+
+			int[] A = new int[] { 1, 5, 2 };
+
+			int n = 100000;
+			const int max_int = 2147483647;
+			Random rand = new Random();
+
+			int[] arr = new int[n];
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = rand.Next(0, max_int);
+			}
+
+
+			var result = calc.GetOccurrences(arr);
+
+			Console.WriteLine(result);
+			Console.ReadKey();
+			
 		}
 	}
 }
